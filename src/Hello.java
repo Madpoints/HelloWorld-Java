efe2e4e10bc49cd8cfc92259a2e29f097e307746
John Furtado <jf8846@gmail.com>
2017-12-15 19:48:30 -0500

    Java variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,17 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = 10 + 5 + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int i = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(i);
     }
 }
